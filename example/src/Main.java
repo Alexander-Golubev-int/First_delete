8d0d6e2a2c781de4f05a69051cd967e3a9e9586b
Alexander Golubev <a.golubevspb1608@gmail.com>
2026-02-19 00:16:26 +0300

    Первый коммит

---
+++ example/src/Main.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+
+
+    System.out.println("Hello world!");
+}
